30056d61653493d0f4cf62939300b3124fc3d4a6
abduljalilpalala <abduljalil.palala@sun-asterisk.com>
2022-10-09 14:23:50 +0800

    [SATS-088] - [FE] Integrate User Login Functionality

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -4,19 +4,18 @@ import { NextPage } from 'next'
 import { SignInUpFormValues } from '~/shared/types'
 import AuthForm from '~/components/molecules/AuthForm'
 import AuthLayout from '~/components/templates/AuthLayout'
+import useAuth from '~/hooks/auth'
 
 const Login: NextPage = (): JSX.Element => {
+  const { login, isError, error } = useAuth()
+
   const handleAuthSubmit = async (data: SignInUpFormValues): Promise<void> => {
-    return new Promise((resolve) => {
-      setTimeout(() => {
-        resolve()
-      }, 3000)
-    })
+    await login(data)
   }
 
   return (
     <AuthLayout metaTitle="Registration">
-      <AuthForm isLogin actions={{ handleAuthSubmit }} />
+      <AuthForm isLogin actions={{ handleAuthSubmit }} axiosErrors={{ isError, error }} />
     </AuthLayout>
   )
 }
