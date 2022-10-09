6d3dba73f94225ed39aaa239fc3cf02ac5518eca
unknown <jsvelte>
2022-10-09 15:44:45 +0800

    [SATS-107] - [Markup] Create Admin Dashboard

--- client/src/pages/admin/dashboard.tsx
+++ client/src/pages/admin/dashboard.tsx
@@ -7,7 +7,40 @@ import AdminLayout from '~/components/templates/AdminLayout'
 const Dashboard: NextPage = (): JSX.Element => {
   return (
     <AdminLayout metaTitle="Administrator | Dashboard">
-      <UnderConstruction />
+      <div className="grid grid-cols-2 gap-x-3">
+        <a
+          href="#"
+          className="flex flex-col items-center rounded-lg border bg-white shadow-md hover:bg-slate-100 md:max-w-xl md:flex-row"
+        >
+          <img
+            className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            src="/docs/images/blog/image-4.jpg"
+            alt=""
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-slate-900">
+              Number of Post
+            </h5>
+            <p className="mb-3 font-normal text-slate-700">10000+</p>
+          </div>
+        </a>
+        <a
+          href="#"
+          className="flex flex-col items-center rounded-lg border bg-white shadow-md hover:bg-slate-100 md:max-w-xl md:flex-row"
+        >
+          <img
+            className="h-96 w-full rounded-t-lg object-cover md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            src="/docs/images/blog/image-4.jpg"
+            alt=""
+          />
+          <div className="flex flex-col justify-between p-4 leading-normal">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-slate-900">
+              Number of Alumni
+            </h5>
+            <p className="mb-3 font-normal text-slate-700">3000+</p>
+          </div>
+        </a>
+      </div>
     </AdminLayout>
   )
 }
