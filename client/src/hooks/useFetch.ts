0be696b6b4caf6c22c150988a34f3a1d13da23fd
John Paul Banera <104751512+johnpaul-sun@users.noreply.github.com>
2022-10-31 11:27:13 +0800

    [SATS-896] - [FE] Setup SWR (#20)

---
+++ client/src/hooks/useFetch.ts
@@ -0,0 +1,22 @@
+import axios, { AxiosResponse } from 'axios'
+import useSWR from 'swr' 
+
+export const useFetch = (url: string | null) => {
+  const fetcher = async (url: string): Promise<any> =>
+    await axios(url).then((res: AxiosResponse) => res.data)
+
+  const options = {
+    revalidateIfStale: false,
+    revalidateOnFocus: false,
+    revalidateOnReconnect: true,
+    revalidateOnMount: true,
+  }
+
+  const { data, error, isValidating } = useSWR(url, fetcher, options)
+
+  return {
+    response: data,
+    isLoading: !error && !data,
+    isValidating,
+  }
+}
