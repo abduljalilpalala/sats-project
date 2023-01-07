d60461d029886ec3a934b48338f6ede1ad122e00
abduljalilpalala <abduljalil.palala@sun-asterisk.com>
2023-01-07 18:59:48 +0800

    [SAMS-475] - [BE] Update API for updating employment status

--- api/app/Http/Controllers/UpdateUserController.php
+++ api/app/Http/Controllers/UpdateUserController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UpdateUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 
 class UpdateUserController extends Controller
 {
     public function __invoke(UpdateUserRequest $request)
     {
         auth()->user()->updateUserDetails($request);
-        return response()->json(auth()->user());
+        return new UserResource(User::with(['avatar', 'batch', 'course', 'job.iDImage'])->findOrFail(auth()->user()->id));
     }
 }
--- api/app/Http/Requests/UpdateUserRequest.php
+++ api/app/Http/Requests/UpdateUserRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Enums\EmploymentStatusEnum;
 use Illuminate\Foundation\Http\FormRequest;
 
 class UpdateUserRequest extends FormRequest
@@ -24,13 +25,24 @@ class UpdateUserRequest extends FormRequest
     public function rules()
     {
         return [
+            'id_number' => ['nullable', 'max:255'],
             'name' => ['required', 'max:255'],
             'email' => ['required', 'email', 'max:255', "unique:users,email," . $this->request->get('id') . ""],
             'contact_number' => ['required', 'min:11', 'max:11'],
-            'id_number' => ['nullable', 'max:255'],
+            'batch_id' => ['required'],
             'birth_date' => ['required', 'date', 'max:255'],
-            'employment_status_id' => ['nullable'],
-            'course_id' => ['nullable']
+            'employment_status_id' => ['required', "in:" . EmploymentStatusEnum::EMPLOYED->value . "," . EmploymentStatusEnum::UNEMPLOYED->value . "," . EmploymentStatusEnum::SELF_EMPLOYED->value . ""],
+            'course_id' => ['nullable'],
+            'job_id' => ['nullable'],
+            'work_place' => ['nullable', 'max:255'],
+            'company_name' => ['nullable', 'max:255'],
+            'position' => ['nullable', 'max:255'],
+            'work_id' => ['mimes:jpeg,jpg,png', 'max:2048']
         ];
+    }
+
+    public function allowed()
+    {
+        return $this->except(['job_id', 'work_place', 'position', 'work_id', 'company_name']);
     }
 }
